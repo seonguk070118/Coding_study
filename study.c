b60d78395005054637619c7bd75661e15c221920
seonguk070118 <seonguk070118@gmail.com>
2023-07-15 22:38:49 +0900

    10869_baekjoon

--- study.c
+++ study.c
@@ -6,13 +6,13 @@
 
 int main()
 {
-	int a;
-	scanf("%d",&a);
-	if(a>=90) printf("A");
-	else if(a>=80) printf("B");
-	else if(a>=70) printf("C");
-	else if(a>=60) printf("D");
-	else printf("F");
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%d\n",a+b);
+	printf("%d\n",a-b);
+	printf("%d\n",a*b);
+	printf("%d\n",a/b);
+	printf("%d\n",a%b);
 	return 0;
 }
 

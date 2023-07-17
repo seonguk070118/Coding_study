f6159d5c49a98137660415c87a0c9a2226785b1e
seonguk070118 <seonguk070118@gmail.com>
2023-07-17 16:37:49 +0900

    1226_codeup

--- study.c
+++ study.c
@@ -6,12 +6,45 @@
 
 int main()
 {
-	float a,b,c,d,A,B;
-	scanf("%d %d %d %d",&a,&b,&c,&d);
-	A=a/b;
-	B=c/d;
-	if(A>B) printf(">");
-	else if(A<B) printf("<");
-	else printf("=");
+	int a[8];
+	int b[7],i,sum=0,bo=0;
+	for(i=0;i<7;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<6;i++)
+	{
+		scanf("%d",&b[i]);
+	}
+	for(i=0;i<6;i++){
+		if(a[6]==b[i]){
+			bo++;
+		}
+		if(a[0]==b[i]){
+			sum++;
+		}
+		if(a[1]==b[i]){
+			sum++;
+		}
+		if(a[2]==b[i]){
+			sum++;
+		}
+		if(a[3]==b[i]){
+			sum++;
+		}
+		if(a[4]==b[i]){
+			sum++;
+		}
+		if(a[5]==b[i]){
+			sum++;
+		}
+	}
+	if(sum==6) printf("1");
+	else if(sum==5 && bo==1) printf("2");
+	else if(sum==5) printf("3");
+	else if(sum==4) printf("4");
+	else if(sum==3) printf("5");
+	else if(sum<=2) printf("0");
+	return 0;
 }
 

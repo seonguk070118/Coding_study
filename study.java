960e2d35dd75ef0b96d5e1a1d8b3b8580180c91a
seonguk070118 <seonguk070118@gmail.com>
2023-07-17 15:52:35 +0900

    book_4

--- study.java
+++ study.java
@@ -2,10 +2,22 @@ package mypack;
 
 public class study {
 	public static void main(String[] ar) {
-		int a=3;
-		int b;
-		b=4;
-		System.out.println(a);
-		System.out.println(b);
+		//변수
+		boolean aBcD;
+		byte 가나다;
+		short _abcd;
+		// int 3abcd;
+		char $ab_cd;
+		// float int;
+		double main;
+		//int my Work;
+		String myClassName;
+		int ABC;
+		
+		
+		//상수
+		final double PI;
+		final int MY_DATA;
+		final float myData;
 	}
 }

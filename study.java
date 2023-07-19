7043b67415229b1f38372bb8e92162572e8fbdf6
seonguk070118 <seonguk070118@gmail.com>
2023-07-19 21:15:01 +0900

    book_6

--- study.java
+++ study.java
@@ -2,14 +2,16 @@ package mypack;
 
 public class study {
 	public static void main(String[] ar) {
-		int value1=3;
-		{
-			int value2=5;
-			System.out.println(value1);
-			System.out.println(value2);
-		}
+		// float의 정밀도(대략 소수 7자리)
+		float f1 = 1.0000001f;
+		System.out.println(f1);
+		float f2 = 1.00000001f;
+		System.out.println(f2);
 		
-		System.out.println(value1);
-		//System.out.println(value2); //오류
+		// double의 정밀도(대략 소수 15자리)
+		double d1 = 1.000000000000001;
+		System.out.println(d1);
+		double d2 = 1.0000000000000001;
+		System.out.println(d2);
 	}
 }

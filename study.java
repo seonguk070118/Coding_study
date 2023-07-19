f33fc13f03c4cd462e100d7df086b6924eb49cdb
seonguk070118 <seonguk070118@gmail.com>
2023-07-19 21:49:17 +0900

    book_9

--- study.java
+++ study.java
@@ -2,31 +2,40 @@ package mypack;
 
 public class study {
 	public static void main(String[] ar) {
-		// 문자로 저장하는 방법
-		char value1 = 'A';
-		char value2 = '가';
-		char value3 = '3';
+		// 같은 자료현 간의 연산
+		int value1 = 3+5;
+		int value2 = 8/5; // int / int = int
+		float value3 = 3.0f + 5.0f;
+		double value4 = 8.0 / 5.0;
+		
+		byte data1 = 3;
+		byte data2 = 5;
+		//byte value5 = data1 + data2; (data1+data2는 int형이므로 오류 발생(byte=int형태)
+		int value5 = data1 + data2;
+		
 		System.out.println(value1);
 		System.out.println(value2);
 		System.out.println(value3);
-		System.out.println();
-		
-		// 정수로 저장하는 방법
-		char value4 = 65;
-		char value5 = 0xac00;
-		char value6 = 51;
 		System.out.println(value4);
 		System.out.println(value5);
-		System.out.println(value6);
 		System.out.println();
 		
-		//유니코드로 직접 입력
-		char value7 = '\u0041';
-		char value8 = '\uac00';
-		char value9 = '\u0033';
+		// 다른 자료형 간의 연산
+		// int value6 = 5 + 3.5; (5+3.5는 double형이므로 오류발생(int = double 형태))
+		double value6 = 5 + 3.5;
+		int value7 = 5 + (int)3.5;
+		
+		double value8 = 5/2.0;
+		byte data3 = 3;
+		byte data4 = 5;
+		int value9 = data3 + data4;
+		double value10 = data3 + data4; //(data3 + data4는 int형이므로 자동타입변환수행)
+		
+		System.out.println(value6);
 		System.out.println(value7);
 		System.out.println(value8);
 		System.out.println(value9);
+		System.out.println(value10);
 		System.out.println();
 	}
 }

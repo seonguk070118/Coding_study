5758387f488e5200de65f7376c15dbfa5441e4f9
seonguk070118 <seonguk070118@gmail.com>
2023-07-19 16:40:49 +0900

    book_Q9

--- study.py
+++ study.py
@@ -1,5 +1,6 @@
-i=0
-while True:
-    i+=1
-    if i>5: break
-    print('*'*i)
+A=[70,60,55,75,95,90,80,80,85,100]
+total=0
+for score in A:
+    total += score
+average=total/len(A)
+print(average)

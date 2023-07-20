cb016a753acb2e43ea501c1e762f1b5f4255154e
seonguk070118 <seonguk070118@gmail.com>
2023-07-20 17:50:18 +0900

    1283_codeup

--- study.c
+++ study.c
@@ -6,17 +6,26 @@
 
 int main()
 {
-	int i,n,k,j;
-	scanf("%d",&n);
-	for(i=1;;i++)
+	double a=0,b,a1=0,c,i,a2=0;
+	scanf("%lf %lf",&a,&b);
+	a1=a;
+	a2=a;
+	for(i=0;i<b;i++)
 	{
-		k=n-i;
-		for(j=k;j>=0;j--){
-			if(j*j==k)
-			{
-				printf("%d %d",i,j);
-				return 0;
-			}
+		scanf("%lf",&c);
+		if(c<0){
+			c*=-1;
+			a2=a1/100*c;
+			a1-=a2;
+		}
+		else{
+			a2=a1/100*c;
+			a1+=a2;
 		}
 	}
+	printf("%.0f",a1-a);
+	if(a1-a>0) printf("good");
+	else if(a1-a<0) printf("bad");
+	else printf("same");
+	return 0;
 }

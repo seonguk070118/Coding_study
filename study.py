0946728ea15fe541ade5820996b58e8f32eccad3
seonguk070118 <seonguk070118@gmail.com>
2023-07-20 21:56:59 +0900

    book_Q13

--- study.py
+++ study.py
@@ -1,7 +1,4 @@
-def avg_numbers(*args):
-    result = 0
-    for i in args:
-        result += i
-    return print(result/len(args))
-avg_numbers(1,2)
-avg_numbers(1,2,3,4,5)
+input1 = input("첫번째 숫자:")
+input2 = input("두번째 숫자:")
+total = int(input1) + int(input2)
+print("%s"%total)

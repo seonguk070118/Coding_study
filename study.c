cdc38aa7122f9a3dbedd2c91eae6e4f88e25565e
seonguk070118 <seonguk070118@gmail.com>
2023-07-21 18:49:32 +0900

    1356_codeup

--- study.c
+++ study.c
@@ -3,29 +3,27 @@
 #include<limits.h>
 #include<string.h>
 #include <stdbool.h>
-
 int main()
 {
-	double a=0,b,a1=0,c,i,a2=0;
-	scanf("%lf %lf",&a,&b);
-	a1=a;
-	a2=a;
-	for(i=0;i<b;i++)
+	int n,i,j;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
 	{
-		scanf("%lf",&c);
-		if(c<0){
-			c*=-1;
-			a2=a1/100*c;
-			a1-=a2;
-		}
-		else{
-			a2=a1/100*c;
-			a1+=a2;
-		}
+		printf("*");
 	}
-	printf("%.0f",a1-a);
-	if(a1-a>0) printf("good");
-	else if(a1-a<0) printf("bad");
-	else printf("same");
+	printf("\n");
+	for(i=0;i<n-2;i++)
+	{
+		printf("*");
+		for(j=0;j<n-2;j++)
+		{
+			printf(" ");
+		}
+		printf("*\n");
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("*");
+	}
 	return 0;
 }

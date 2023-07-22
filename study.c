d7180c2e72da3f18f36e6cf62227b406f72243bc
seonguk070118 <seonguk070118@gmail.com>
2023-07-22 20:54:43 +0900

    1357_codeup

--- study.c
+++ study.c
@@ -6,24 +6,20 @@
 int main()
 {
 	int n,i,j;
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-		printf("*");
-	}
-	printf("\n");
-	for(i=0;i<n-2;i++)
-	{
-		printf("*");
-		for(j=0;j<n-2;j++)
-		{
-			printf(" ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+    	for(j=0;j<i;j++)
+    	{
+    		printf("*");
 		}
-		printf("*\n");
+		printf("\n");
 	}
-	for(i=0;i<n;i++)
-	{
-		printf("*");
+	for(i=n-1;i>=1;i--){
+    	for(j=i;j>=1;j--){
+    		printf("*");
+		}
+		printf("\n");
 	}
 	return 0;
 }

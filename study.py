16e2984d506e7509788fba2e984098eee8e1739f
seonguk070118 <seonguk070118@gmail.com>
2023-07-23 21:23:52 +0900

    book_5

--- study.py
+++ study.py
@@ -1,4 +1,28 @@
-input1 = input("첫번째 숫자:")
-input2 = input("두번째 숫자:")
-total = int(input1) + int(input2)
-print("%s"%total)
+class FourCal:
+    def setdata(self,first,second):
+        self.first=first
+        self.second=second
+    def add(self):
+        result=self.first+self.second
+        return result
+    def mul(self):
+        result=self.first*self.second
+        return result
+    def sub(self):
+        result=self.first-self.second
+        return result
+    def div(self):
+        result=self.first/self.second
+        return result
+a=FourCal()
+b=FourCal()
+a.setdata(4,2)
+b.setdata(3,8)
+print(a.add())
+print(a.mul())
+print(a.sub())
+print(a.div())
+print(b.add())
+print(b.mul())
+print(b.sub())
+print(b.div())

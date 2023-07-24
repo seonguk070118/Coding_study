fb01c32bb2e53f0ca1f16fdd5dc8e8a1ea81df11
seonguk070118 <seonguk070118@gmail.com>
2023-07-24 19:10:33 +0900

    book_Q1

--- study.java
+++ study.java
@@ -2,13 +2,10 @@ package mypack;
 
 public class study {
 	public static void main(String[] ar) {
-		int score=72;
-		switch(score/10){
-			case 10:
-			case 9: System.out.println("A"); break;
-			case 8: System.out.println("B"); break;
-			case 7: System.out.println("C"); break;
-			default: System.out.println("F");
+		int i=10;
+		while(i>0) {
+			System.out.println(i);
+			i-=2;
 		}
 	}
 }

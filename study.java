d5fc10bbf916f7b71c5a70a21e96232311738623
seonguk070118 <seonguk070118@gmail.com>
2023-07-24 18:53:39 +0900

    book_14

--- study.java
+++ study.java
@@ -2,14 +2,14 @@ package mypack;
 
 public class study {
 	public static void main(String[] ar) {
-		out:
+		POS1:
 		for(int i=0;i<10;i++)
 		{
 			for(int j=0;j<10;j++)
 			{
 				if(j==3)
 				{
-					break out;
+					continue POS1;
 				}
 				System.out.println(i+","+j);
 			}

c6a33078cb55949d38819539b4155961a1437a4a
seonguk070118 <seonguk070118@gmail.com>
2023-07-24 14:14:19 +0900

    1407_codeup

--- study.c
+++ study.c
@@ -5,21 +5,14 @@
 #include <stdbool.h>
 int main()
 {
-	int n,i,j;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-    	for(j=0;j<i;j++)
-    	{
-    		printf("*");
+	char a[101];
+	int i;
+	gets(a);
+	for(i=0;a[i]!=0;i++)
+	{
+		if(a[i]!=' '){
+			printf("%c",a[i]);
 		}
-		printf("\n");
-	}
-	for(i=n-1;i>=1;i--){
-    	for(j=i;j>=1;j--){
-    		printf("*");
-		}
-		printf("\n");
 	}
 	return 0;
 }

072f6729bac03f47bed73c812ced889d30e16cb5
seonguk070118 <seonguk070118@gmail.com>
2023-07-25 15:54:32 +0900

    1358_codeup

--- study.c
+++ study.c
@@ -3,44 +3,24 @@
 #include<limits.h>
 #include<string.h>
 #include <stdbool.h>
-int a,b,t[100]={0},i,j=0,k=0;
-
-void change()
-{
-	int n;
-	for(j=0;j<a;j++)
-	{
-		for(i=0;i<a-1-j;i++)
-		{
-			if(t[i]>=t[i+1])
-			{
-				n=t[i];
-				t[i]=t[i+1];
-				t[i+1]=n;
-			}
-		}
-	}
-}
 
 int main()
 {
-	scanf("%d %d",&a,&b);
-	for(i=0;i<a;i++)
+	int a,i,j;
+	scanf("%d",&a);
+	int k=a/2;
+	for(i=1;i<=a;i+=2)
 	{
-		scanf("%d",&t[i]);
-	}
-	change();
-	for(i=0;i<a;i++)
-	{
-		for(j=0;j<b;j++)
+		for(j=k;j>=1;j--)
 		{
-			if(t[k]!=0)
-			{
-				printf("%d ",t[k]);
-				k++;
-			}
+			printf(" ");
+		}
+		for(j=0;j<i;j++)
+		{
+			printf("*");
 		}
 		printf("\n");
+		k--;
 	}
 	return 0;
 }

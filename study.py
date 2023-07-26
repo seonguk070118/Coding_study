5ea652bfcb019b82a999ea9daef2a14e59031306
seonguk070118 <seonguk070118@gmail.com>
2023-07-26 13:41:22 +0900

    book_9

--- study.py
+++ study.py
@@ -1,8 +1,5 @@
-def GuGu(n):
-    result=[]
-    i=1
-    while i<10:
-        result.append(n*i)
-        i+=1
-    return result
-print(GuGu(2))
+result=0
+for n in range(1,1000):
+    if n%3==0 or n%5==0:
+        result+=n
+print(result)

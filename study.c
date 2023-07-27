24d0781acaaaf609db95758593b98fd437127dbc
seonguk070118 <seonguk070118@gmail.com>
2023-07-27 14:31:25 +0900

    1539_codeup

--- study.c
+++ study.c
@@ -3,37 +3,19 @@
 #include<limits.h>
 #include<string.h>
 #include <stdbool.h>
+int n;
+
+void f()
+{
+	if(n==0)
+		printf("false");
+	else
+		printf("true");
+}
+
 int main()
 {
-	int n,i,j,k;
-	scanf("%d",&n);
-	for(i=2;i<=n;i++)
-	{
-		if(n%i==0)
-		{
-			for(j=2;j<=(i)/2;j++)
-			{
-				if(i%j==0)
-				{
-					break;
-				}
-			}
-			
-			for(k=2;k<=(n/i)/2;k++)
-			{
-				if((n/i)%k==0)
-				{
-					break;
-				}
-			}
-			
-			if(j==i/2+1&&k==(n/i)/2+1)
-			{
-				printf("%d %d",i,n/i);
-				return 0;
-			}
-		}
-	}
-	printf("wrong number");
-	return 0;
+  scanf("%d", &n);
+  f(n);
+  return 0;
 }

b37fc420935056ba35e6d8cc18ef17f1b29e64c2
seonguk070118 <seonguk070118@gmail.com>
2023-08-01 02:27:42 +0900

    2753_baekjoon

--- study.py
+++ study.py
@@ -1,11 +1,7 @@
 a=int(input())
-if a>=90:
-    print('A')
-elif a>=80:
-    print('B')
-elif a>=70:
-    print('C')
-elif a>=60:
-    print('D')
-else:
-    print('F')
+if a%4 == 0 and a%100 != 0:
+    print(1)
+elif a%400==0:
+    print(1)
+else :
+    print(0)

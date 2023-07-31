91c0274fc947f59c9f7653b1a8cb48a154aaae8d
seonguk070118 <seonguk070118@gmail.com>
2023-07-31 17:22:38 +0900

    1285_codeup

--- study.c
+++ study.c
@@ -7,8 +7,34 @@
 
 int main()
 {
-  int a;
+  int sum=0,i,a;
+  char b;
   scanf("%d",&a);
-  printf("%d",a-543);
+  sum=a;
+  for(i=0;;i++)
+  {
+	scanf("%c%d",&b,&a);
+  	if(b=='+')
+  	{
+  		sum+=a;
+	}
+	else if(b=='-')
+	{
+		sum-=a;
+	}
+	else if(b=='*')
+	{
+		sum*=a;
+	}
+	else if(b=='/')
+	{
+		sum/=a;
+	}
+	else if(b=='=')
+	{
+		break;
+	}
+  }
+  printf("%d",sum);
   return 0;
 }

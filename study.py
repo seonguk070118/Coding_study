b7c499bcebe74c9c2be3832bb0b90737874c9db1
seonguk070118 <seonguk070118@gmail.com>
2023-08-01 02:22:59 +0900

    9498_baekjoon

--- study.py
+++ study.py
@@ -1,7 +1,11 @@
-a, b=input().split()
-if int(a)<int(b):
-    print('<')
-elif int(a)>int(b):
-    print('>')
+a=int(input())
+if a>=90:
+    print('A')
+elif a>=80:
+    print('B')
+elif a>=70:
+    print('C')
+elif a>=60:
+    print('D')
 else:
-    print('==')
+    print('F')

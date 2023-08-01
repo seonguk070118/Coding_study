859215d81364bf3946e164b57cd2d971048503c8
seonguk070118 <seonguk070118@gmail.com>
2023-08-01 12:37:38 +0900

    2439_baekjoon

--- study.py
+++ study.py
@@ -2,6 +2,8 @@ a=int(input())
 for i in range(a+1):
     if i==0:
         continue
-    for i in range(i):
+    for k in range(a-i):
+        print(' ',end='')
+    for j in range(i):
         print('*',end='')
     print()

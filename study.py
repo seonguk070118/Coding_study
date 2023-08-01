b788deca016614f461d3b74a14c6e68ee9c4fa40
seonguk070118 <seonguk070118@gmail.com>
2023-08-01 12:29:25 +0900

    2739_baekjoon

--- study.py
+++ study.py
@@ -1,22 +1,7 @@
-a,b,c=input().split()
-a=int(a)
-b=int(b)
-c=int(c)
-if a==b==c:
-    sum=10000+(a*1000)
-elif a==b!=c:
-    sum=1000+(a*100)
-elif a!=b==c:
-    sum=1000+(b*100)
-elif a==c!=b:
-    sum=1000+(c*100)
-else:
-    if a>b:
-        max=a
-    else:
-        max=b
-    if max>c:
-        sum=max*100
-    else:
-        sum=c*100
-print(sum)
+a=input()
+for i in range(10):
+    if i==0:
+        continue
+    print(a+' * '+str(i)+' =',end=' ')
+    b=int(a)*int(i)
+    print(b)

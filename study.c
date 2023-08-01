a6c6107ea1307609edd1e9a5f09659f137dad183
seonguk070118 <seonguk070118@gmail.com>
2023-08-01 13:23:40 +0900

    1294_codeup

--- study.c
+++ study.c
@@ -3,19 +3,27 @@
 #include<limits.h>
 #include<string.h>
 #include <stdbool.h>
-
+#include <ctype.h>
 
 int main()
 {
-  int a,i,j;
-  scanf("%d",&a);
-  for(i=1;i<=9;i++)
-  {
-  	for(j=0;j<a*i;j++)
-  	{
-  		printf("*");
+	int i;
+	char a[100]={0};
+	gets(a);
+	for(i=0;i<sizeof(a);i++)
+	{
+		if(a[i]==0)
+			break;
+		if(isspace(a[i]))
+			printf(" ");
+		else if(a[i]=='x')
+			printf("a");
+		else if(a[i]=='y')
+			printf("b");
+		else if(a[i]=='z')
+			printf("c");
+		else
+			printf("%c",a[i]+3);
 	}
-	printf("\n");
-  }
-  return 0;
+	return 0;
 }

4c13c7f39e8f6b78a0427a60d7397298fed01381
seonguk070118 <seonguk070118@gmail.com>
2023-08-01 12:35:02 +0900

    2438_baekjoon

--- study.py
+++ study.py
@@ -1,5 +1,7 @@
 a=int(input())
-b=int(a/4)
-for i in range(b):
-    print('long',end=' ')
-print('int')
+for i in range(a+1):
+    if i==0:
+        continue
+    for i in range(i):
+        print('*',end='')
+    print()

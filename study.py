f71786589868412838d453424385cd604cc2a6ee
seonguk070118 <seonguk070118@gmail.com>
2023-08-01 12:06:31 +0900

    14681_baekjoon

--- study.py
+++ study.py
@@ -1,7 +1,10 @@
 a=int(input())
-if a%4 == 0 and a%100 != 0:
-    print(1)
-elif a%400==0:
-    print(1)
+b=int(input())
+if a>0 and b>0:
+    print('1')
+elif a<0 and b>0:
+    print('2')
+elif a<0 and b<0:
+    print('3')
 else :
-    print(0)
+    print('4')

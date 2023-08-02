820a85ebe0339d5e3c6e5ceda34921874080e4d9
seonguk070118 <seonguk070118@gmail.com>
2023-08-02 17:54:29 +0900

    1546_codeup

--- study.c
+++ study.c
@@ -13,10 +13,18 @@ int zero()
 	    return 0;
 }
 
+int plus()
+{
+	if(n>0)
+		return 1;
+	else
+	    return 0;
+}
+
 int main()
 {
 	scanf("%d", &n);
 	if(zero(n)) printf("zero");
-  	else printf("non zero");
+	else printf("%s", plus(n)?"plus":"minus");
 	return 0;
 }

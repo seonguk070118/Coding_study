6e79270a2ae47868668c84c8f9570edec4a63db4
seonguk070118 <seonguk070118@gmail.com>
2023-08-14 17:35:22 +0900

    1567_codeup

--- study.c
+++ study.c
@@ -3,25 +3,23 @@
 #include<limits.h>
 #include<string.h>
 #include <stdbool.h>
+int n,a,b,d[1010];
 
+int subsetsum(){
+	long long int i,sum=0;
+	for(i=a;i<=b;i++)
+	{
+		sum+=d[i];
+	}
+	return sum;
+}
 
 int main()
 {
-	int a,i,b,c,sum=0,d;
-	scanf("%d",&d);
-	scanf("%d",&a);
-	for(i=0;i<a;i++)
-	{
-		scanf("%d %d",&b,&c);
-		sum+=b*c;
-	}
-	if(sum==d)
-	{
-		printf("Yes");
-	}
-	else
-	{
-		printf("No");
-	}
-	return 0;
+	int i;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++)
+		scanf("%d", &d[i]);
+	scanf("%d%d", &a, &b);
+	printf("%lld\n", subsetsum(a, b));
 }

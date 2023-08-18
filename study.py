6d5cb185572c019062d07a566b818828d52f2984
seonguk070118 <seonguk070118@gmail.com>
2023-08-18 22:42:55 +0900

    9085_baekjoon

--- study.py
+++ study.py
@@ -1,14 +1,4 @@
-a=[]
-max=0
-ind=1
-maxi=0
-for i in range(9):
-    i=int(input())
-    a.append(i)
-for i in a:
-    if(i>max):
-        max=i
-        maxi=ind
-    ind=ind+1
-print(max)
-print(maxi)
+a=int(input())
+for i in range(a):
+    b=int(input())
+    print(sum(list(map(int,input().split()))))

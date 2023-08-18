08699c5cc528fecda5795783b43f75f7920f7c90
seonguk070118 <seonguk070118@gmail.com>
2023-08-18 22:33:49 +0900

    2562_baekjoon

--- study.py
+++ study.py
@@ -1,7 +1,14 @@
-a=int(input())
-b=a
-for i in range(a):
-    for j in range(b):
-        print('*',end='')
-    print()
-    b=b-1
+a=[]
+max=0
+ind=1
+maxi=0
+for i in range(9):
+    i=int(input())
+    a.append(i)
+for i in a:
+    if(i>max):
+        max=i
+        maxi=ind
+    ind=ind+1
+print(max)
+print(maxi)

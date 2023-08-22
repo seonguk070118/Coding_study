c25526ce40eaff8b7caa954da3505a5167feb48c
seonguk070118 <seonguk070118@gmail.com>
2023-08-22 12:08:57 +0900

    random_game_Exception_Handling

--- study.py
+++ study.py
@@ -2,13 +2,16 @@ import random
 count=0
 rn=random.randint(1,100)
 while 1:
-    a=int(input())
-    count+=1
-    if a==rn:
-        print(count)
-        break
-    else :
-        if a>rn:
-            print('down')
+    try :
+        a=int(input())
+        count+=1
+        if a==rn:
+            print(count)
+            break
         else :
-            print('up')
+            if a>rn:
+                print('down')
+            else :
+                print('up')
+    except :
+        print('숫자를 입력하세요')

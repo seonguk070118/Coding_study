9c6854f98847fcac28ac0c79dfea02562436436e
seonguk070118 <seonguk070118@gmail.com>
2023-08-22 23:24:04 +0900

    Crawling_rank and link

--- clubstudy.py
+++ clubstudy.py
@@ -1,12 +1,23 @@
+import urllib.request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 response=urlopen("https://news.naver.com/main/ranking/popularMemo.naver")
 soup=BeautifulSoup(response, "html.parser")
 
-value=soup.find("a",{"class": "list_title nclicks('RBP.cmtnws')"})
-print(value)
+title=soup.find("a",{"class": "list_title nclicks('RBP.cmtnws')"})
+title2=title.text.strip()
+print(title2[0:])
 
-value2=value.text.strip()
-print(value2[0:])
+rank=soup.find("em",{"class":"list_ranking_num"})
+rank2=rank.text.strip()
+print(rank2)
+
+url = "https://news.naver.com/main/ranking/popularMemo.naver"
+req = urllib.request.Request(url)
+sourcecode = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(sourcecode, "html.parser")
+
+href=soup.find("div", class_="list_content")
+print(href.find("a")["href"])
 

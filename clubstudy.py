a17cd7c3c87be4fa91191730e560c8cb9c1291f2
seonguk070118 <seonguk070118@gmail.com>
2023-08-23 22:33:11 +0900

    Crawling_img_finish

--- clubstudy.py
+++ clubstudy.py
@@ -1,9 +1,17 @@
+import dload
 import urllib.request
+import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 response=urlopen("https://news.naver.com/main/ranking/popularMemo.naver")
 soup=BeautifulSoup(response, "html.parser")
+
+img1=soup.find("a",{"class": "list_img nclicks('RBP.cmtnws')"})
+img=img1.find("img")
+img2=img.get("src")
+print(img2)
+dload.save(img2)
 
 title=soup.find("a",{"class": "list_title nclicks('RBP.cmtnws')"})
 title2=title.text.strip()

f0c9517b9b99b16e4732022c9bedaba1b44591e7
seonguk070118 <seonguk070118@gmail.com>
2023-08-23 10:30:30 +0900

    2진수

--- study.c
+++ study.c
@@ -4,10 +4,16 @@
 #include<string.h>
 #include <stdbool.h>
 
+void f(int n)
+{
+	if(n>1) f(n/2);
+	printf("%d",n%2);
+}
+
 int main()
 {
-	char a[51];
-	scanf("%s",&a);
-	printf("%s??!",a);
+	int n;
+	scanf("%d",&n);
+	f(n);
 	return 0;
 }

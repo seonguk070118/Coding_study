509a5b85998c431938e97345dac7c6b386f545ab
seonguk070118 <seonguk070118@gmail.com>
2023-08-28 16:03:38 +0900

    8/28_1

--- study.py
+++ study.py
@@ -1,2 +1,8 @@
-a,b=map(float,input().split())
-print(a**b)
+n=int(input())
+a=[int(x) for x in input().split()]
+b=list(0 for _ in range(24))
+for i in a:
+    b[i]+=1
+
+for i in range(1,24):
+    print(b[i],end=' ')

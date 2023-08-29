977f93165238443d54daa78dc79e7655c32b13e1
seonguk070118 <seonguk070118@gmail.com>
2023-08-29 12:11:35 +0900

    6093_codeup

--- study.py
+++ study.py
@@ -1,8 +1,4 @@
 n=int(input())
 a=[int(x) for x in input().split()]
-b=list(0 for _ in range(24))
-for i in a:
-    b[i]+=1
-
-for i in range(1,24):
-    print(b[i],end=' ')
+for i in range(n-1,-1,-1):
+    print(a[i],end=' ')

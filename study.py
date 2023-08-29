a2ffa895451a7cbc917efb7cc91917bbeeeadae7
seonguk070118 <seonguk070118@gmail.com>
2023-08-29 12:13:29 +0900

    6094_codeup

--- study.py
+++ study.py
@@ -1,4 +1,7 @@
 n=int(input())
 a=[int(x) for x in input().split()]
-for i in range(n-1,-1,-1):
-    print(a[i],end=' ')
+min=10000
+for i in range(0,n):
+    if(a[i]<min):
+        min=a[i]
+print(min)

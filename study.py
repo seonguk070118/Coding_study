9c2b2ec3e71e013d2207ddc93e058ad46aa20876
seonguk070118 <seonguk070118@gmail.com>
2023-08-29 13:04:03 +0900

    6097_codeup

--- study.py
+++ study.py
@@ -1,21 +1,17 @@
-a=[]
-for i in range(19):
-    a.append(list(map(int,input().split())))
-
+h,w=map(int,input().split())
+a=[[0 for j in range(w)] for i in range(h)]
 n=int(input())
-for i in range(n):
-    x,y=map(int,input().split())
-    for j in range(19):
-        if a[j][y-1]==0:
-            a[j][y-1]=1
-        else:
-            a[j][y-1]=0
-        if a[x-1][j]==0:
-            a[x-1][j]=1
-        else:
-            a[x-1][j]=0
 
-for i in range(19):
-    for j in range(19):
+for i in range(n):
+    l,d,x,y=map(int,input().split())
+    for j in range(l):
+        if d==0:
+            a[x-1][y+j-1]=1
+        else:
+            a[x+j-1][y-1]=1
+
+
+for i in range(h):
+    for j in range(w):
         print(a[i][j],end=' ')
     print()

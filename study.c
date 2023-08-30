b84e49892685da06cc60d0fa90cddd3757e1c970
seonguk070118 <seonguk070118@gmail.com>
2023-08-30 10:18:18 +0900

    Insertion Alignment

--- study.c
+++ study.c
@@ -6,6 +6,19 @@
 
 int main()
 {
+	int list[]={5,3,8,1,2,7};
+	int key,i,j,size,temp
 	
+	size=sizeof(list)/sizeof(int);
+	
+	for(i=1,i<size;i++)
+	{
+		key=list[i];
+		for(j=i-1;j>=0&&list[j]>key;j--)
+		{
+			list[j+1]=list[j];
+		}
+		list[j+1]=key;
+	}
 	return 0;
 }

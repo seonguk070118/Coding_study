0ca94d1e2f1934e968e48758e874dddcd909d744
seonguk070118 <seonguk070118@gmail.com>
2023-09-01 23:01:01 +0900

    6040_codeup

--- study.py
+++ study.py
@@ -1,17 +1,2 @@
-h,w=map(int,input().split())
-a=[[0 for j in range(w)] for i in range(h)]
-n=int(input())
-
-for i in range(n):
-    l,d,x,y=map(int,input().split())
-    for j in range(l):
-        if d==0:
-            a[x-1][y+j-1]=1
-        else:
-            a[x+j-1][y-1]=1
-
-
-for i in range(h):
-    for j in range(w):
-        print(a[i][j],end=' ')
-    print()
+a,b=map(int,input().split())
+print(a//b)

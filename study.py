7a356d43c29ceae996e3e00a324d07b95405615a
seonguk070118 <seonguk070118@gmail.com>
2023-09-11 15:38:48 +0900

    9/11_1

--- study.py
+++ study.py
@@ -1,2 +1,10 @@
-a,b=map(int,input().split())
-print(a==b)
+arr = [[0 for j in range(19)] for i in range(19)]
+n=int(input())
+for i in range(n):
+    x,y=map(int,input().split())
+    arr[x-1][y-1]=1
+
+for i in range(19):
+    for j in range(19):
+        print(arr[i][j],end=' ')
+    print()

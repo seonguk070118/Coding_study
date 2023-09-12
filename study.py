7fb992e20ac739e6dbd4328c66d66f6efcc70c71
seonguk070118 <seonguk070118@gmail.com>
2023-09-12 11:59:01 +0900

    6069_딕셔너리

--- study.py
+++ study.py
@@ -1,10 +1,2 @@
-arr = [[0 for j in range(19)] for i in range(19)]
-n=int(input())
-for i in range(n):
-    x,y=map(int,input().split())
-    arr[x-1][y-1]=1
-
-for i in range(19):
-    for j in range(19):
-        print(arr[i][j],end=' ')
-    print()
+a={'A':'best!!!','B':'good!!','C':'run!','D':'slowly~'}
+print(a.get(input(),'what?'))

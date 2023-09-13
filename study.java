f60929c7e3e1f45977fba7df825d2c83bc5f50ed
seonguk070118 <seonguk070118@gmail.com>
2023-09-13 18:06:41 +0900

    9/13

--- study.java
+++ study.java
@@ -3,17 +3,19 @@ package mypack;
 import java.util.Arrays;
 
 class A{
-	int m=3;
-	void print() {
-		System.out.println("객체 생성 및 활용");
-	}
+	void print(){ System.out.printf("입력값이 없습니다.\n"); }
+	void print(int x){ System.out.printf("정수 입력값: %d\n",x); }
+	void print(double x){ System.out.printf("실수 입력값 %.1f\n",x); }
+	void print(String x){ System.out.printf("문자열 입력값 : %s\n", x); }
+
 }
 
 public class study {
 	public static void main(String[] ar) {
-		A a = new A();
-		a.m = 5;
-		System.out.println(a.m);
+		A a=new A();
 		a.print();
+		a.print(3);
+		a.print(5.8);
+		a.print("안녕");
 	}
 }

257280f93a1a724d53a0f479a67e301acd14b74e
seonguk070118 <seonguk070118@gmail.com>
2023-09-13 09:30:18 +0900

    shell_sort

--- study.c
+++ study.c
@@ -4,26 +4,36 @@
 #include<string.h>
 #include <stdbool.h>
 
+void insertsort(int list[],int first,int last,int gap)
+{
+	int i,j,key;
+	for(i=first+gap;i<=last;i=i+gap)
+	{
+		key=list[i];
+		for(j=i-gap;j>=first&&key<list[j];j=j-gap)
+		{
+			list[j+gap]=list[j];
+		}
+		list[j+gap]=key;
+	}
+}
+
 int main()
 {
-	int list[]={6,5,4,3,2,1};
-	int key,i,j,size,temp;
-	
-	size=sizeof(list)/sizeof(int);
-	
-	for(i=size-1;i>0;i--)
+	int list[]={10,8,6,20,4,3,22,1,0,15,16};
+	int n,i,j,gap;
+	n=sizeof(list)/sizeof(int);
+	for(gap=n/2;gap>0;gap=gap/2)
 	{
-		for(j=0;j<i;j++)
+		if(gap%2==0) gap++;
+		for(i=0;i<gap;i++)
 		{
-			if(list[j]>list[j+1])
-			{
-				temp=list[j+1];
-				list[j+1]=list[j];
-				list[j]=temp;
-			}
+			insertsort(list,i,n-1,gap);
 		}
 	}
-	for(i=0;i<size;i++)
+	for(i=0;i<11;i++)
+	{
 		printf("%d ",list[i]);
+	}
 	return 0;
 }

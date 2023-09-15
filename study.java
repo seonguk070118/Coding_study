844c010b908ebbe04f78702e47b39c4d5890b897
seonguk070118 <seonguk070118@gmail.com>
2023-09-16 00:49:47 +0900

    9/16

--- study.java
+++ study.java
@@ -3,19 +3,22 @@ package mypack;
 import java.util.Arrays;
 
 class A{
-	void print(){ System.out.printf("입력값이 없습니다.\n"); }
-	void print(int x){ System.out.printf("정수 입력값: %d\n",x); }
-	void print(double x){ System.out.printf("실수 입력값 %.1f\n",x); }
-	void print(String x){ System.out.printf("문자열 입력값 : %s\n", x); }
-
+	void averageScore(int...scores) {
+		int sum=0;
+		for(int k:scores) {
+			sum+=k;
+		}
+		double avg=(double)sum/scores.length;
+		System.out.println(avg);
+	}
 }
 
 public class study {
 	public static void main(String[] ar) {
 		A a=new A();
-		a.print();
-		a.print(3);
-		a.print(5.8);
-		a.print("안녕");
+		a.averageScore(1);
+		a.averageScore(1,2);
+		a.averageScore(1,2,3);
+		a.averageScore(1,2,3,4);
 	}
 }

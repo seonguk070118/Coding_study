1d834a8c3fc39e2d31c089e7232eeb8e7107bcd6
seonguk070118 <seonguk070118@gmail.com>
2023-09-16 00:52:49 +0900

    9/16_2

--- study.java
+++ study.java
@@ -3,22 +3,24 @@ package mypack;
 import java.util.Arrays;
 
 class A{
-	void averageScore(int...scores) {
-		int sum=0;
-		for(int k:scores) {
-			sum+=k;
-		}
-		double avg=(double)sum/scores.length;
-		System.out.println(avg);
+	int a,b,c,d;
+	A(){
+		this(5);
+	}
+	A(int k){
+		a=k;
+		b=k;
+		c=k;
+		d=k;
 	}
 }
 
 public class study {
 	public static void main(String[] ar) {
 		A a=new A();
-		a.averageScore(1);
-		a.averageScore(1,2);
-		a.averageScore(1,2,3);
-		a.averageScore(1,2,3,4);
+		System.out.println(a.a);
+		System.out.println(a.b);
+		System.out.println(a.c);
+		System.out.println(a.d);
 	}
 }

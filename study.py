c0acc4f12d6ae45ab6f0f364346acd7ca22273b9
seonguk070118 <seonguk070118@gmail.com>
2023-09-18 15:55:21 +0900

    9/18

--- study.py
+++ study.py
@@ -1,2 +1,11 @@
-a=int(input())
-print(bool(a))
+n=int(input())
+a=[int(input()) for i in range(n)]
+b=[0]*n
+if len(a)<=2:
+    print(sum(s))
+else:
+    b[0]=a[0]
+    b[1]=a[0]+a[1]
+    for i in range(2,n):
+        b[i]=max(b[i-3]+a[i-1]+a[i],b[i-2]+a[i])
+print(b[-1])

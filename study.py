177d6b7afd07a4a2a65e77b31db88d5081ece647
seonguk070118 <seonguk070118@gmail.com>
2023-09-19 11:54:38 +0900

    1901_codeup

--- study.py
+++ study.py
@@ -1,11 +1,6 @@
+def f(n):
+    if n!=0:
+        f(n-1)
+        print(n)
 n=int(input())
-a=[int(input()) for i in range(n)]
-b=[0]*n
-if len(a)<=2:
-    print(sum(s))
-else:
-    b[0]=a[0]
-    b[1]=a[0]+a[1]
-    for i in range(2,n):
-        b[i]=max(b[i-3]+a[i-1]+a[i],b[i-2]+a[i])
-print(b[-1])
+f(n)

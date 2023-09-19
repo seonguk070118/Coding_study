da39f1bd294549394c4ee29dee3920518972318e
seonguk070118 <seonguk070118@gmail.com>
2023-09-19 12:07:43 +0900

    1920_codeup

--- study.py
+++ study.py
@@ -1,6 +1,12 @@
 def f(n):
-    if n==1:
-        return 1
-    return n*f(n-1)
+    if n==0:
+        return
+    f(n//2)
+    if n%2==0:
+        print('0',end='')
+    elif n%2==1:
+        print('1',end='')
 n=int(input())
-print(f(n))
+if n==0:
+        print('0')
+f(n)

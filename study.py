3c7c0149fb1023bdbfd5f9e0994c6627577ec8ad
seonguk070118 <seonguk070118@gmail.com>
2023-09-19 12:02:11 +0900

    1912_codeup

--- study.py
+++ study.py
@@ -1,8 +1,6 @@
-def f(n,m):
-    if n>m:
-        return 0
-    if n%2==1:
-        print(n,end=' ')
-    f(n+1,m)
-n,m=map(int,input().split())
-f(n,m)
+def f(n):
+    if n==1:
+        return 1
+    return n*f(n-1)
+n=int(input())
+print(f(n))

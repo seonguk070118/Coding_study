6dcf0d1c302d38e80b30647ead1ac84785d419a7
seonguk070118 <seonguk070118@gmail.com>
2023-09-21 15:55:01 +0900

    9/21

--- study.c
+++ study.c
@@ -3,54 +3,25 @@
 #include<limits.h>
 #include<string.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-
-int list[]={5,3,8,4,9,1,6,2,7,10,48,26};
-int n=sizeof(list)/sizeof(int);
-
-int partition(int list[],int left,int right)
-{
-	int pivot,low,high,temp;
-	low=left;
-	high=right+1;
-	pivot=list[left];
-	do{
-		do{
-			low++;
-		}while(list[low]<pivot);
-		do{
-			high--;
-		}while(list[high]>pivot);
-		if(low<high)
-		{
-			temp=list[low];
-			list[low]=list[high];
-			list[high]=temp;
-		}
-	}while(low<high);
-	temp=list[left];
-	list[left]=list[high];
-	list[high]=temp;
-	return high;
-}
-
-void quicksort(int list[],int left,int right)
-{
-	if(left<right)
-	{
-		int q=partition(list,left,right);
-		quicksort(list,left,q-1);
-		quicksort(list,q+1,right);
-	}
-}
+typedef struct Item {
+    char name[100];
+    int price;
+    bool limited;
+}Item;
 
 int main()
 {
-	int i;
-	quicksort(list,0,n-1);
-	for(i=0;i<n;i++)
-	{
-		printf("%d ",list[i]);
-	}
-	return 0;
+    struct Item item1 = { "베를린 필하모닉 베토벤 교향곡 전집", 100000, false };
+    Item* ptr=&item1;
+    ptr->limited = true;
+
+    if (ptr->limited == true)
+        printf("한정판\n");
+    else
+        printf("일반판\n");
+
+    return 0;
 }

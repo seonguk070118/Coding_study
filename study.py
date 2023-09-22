d4602c21eaaf93cf196a841f67e9d542d526a47d
seonguk070118 <seonguk070118@gmail.com>
2023-09-22 22:26:06 +0900

    6053_codeup

--- study.py
+++ study.py
@@ -1,8 +1,2 @@
-n=int(input())
-a=list(map(int,input().split()))
-for j in range(n):
-    for i in range(n):
-        print(a[i],end=' ')
-    print()
-    b=a.pop(0)
-    a.append(b)
+a=bool(int(input()))
+print(not a)

a2f82130751086321fa852a846708542bc999c26
seonguk070118 <seonguk070118@gmail.com>
2023-09-26 12:17:08 +0900

    1361_codeup

--- study.py
+++ study.py
@@ -1,11 +1,5 @@
 n=int(input())
-a=list(map(int,input().split()))
-b=list(a)
-c=list(range(n))
 for i in range(n):
-    c[i]=max(a)
-    a.remove(c[i])
-
-for i in b:
-    print(i,end=' ')
-    print(c.index(i)+1)
+    for j in range(i):
+        print(' ',end='')
+    print('**')

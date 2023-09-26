1b08c782309da900779f3be832d36b80bcffb079
seonguk070118 <seonguk070118@gmail.com>
2023-09-26 12:29:08 +0900

    2009_codeup

--- study.py
+++ study.py
@@ -1,14 +1,7 @@
-a=int(input())
-b=int(input())
-c=int(input())
-d=int(input())
-e=int(input())
-arr=[a,b,c]
-arr2=[d,e]
-arr3=[0,0,0,0,0,0]
-k=0
-for i in arr:
-    for j in arr2:
-        arr3[k]=i+j+((i+j)/10)
-        k+=1
-print(min(arr3))
+a,b=map(int,input().split())
+c=0
+while(a>=b):
+    c+=1
+    a-=b
+    a+=1
+print(c)

3fb29c28076c578161fdfc0991e036c91d70b539
seonguk070118 <seonguk070118@gmail.com>
2023-09-26 12:25:10 +0900

    2001_codeup

--- study.py
+++ study.py
@@ -1,5 +1,14 @@
-n=int(input())
-for i in range(n):
-    for j in range(i):
-        print(' ',end='')
-    print('**')
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+arr=[a,b,c]
+arr2=[d,e]
+arr3=[0,0,0,0,0,0]
+k=0
+for i in arr:
+    for j in arr2:
+        arr3[k]=i+j+((i+j)/10)
+        k+=1
+print(min(arr3))

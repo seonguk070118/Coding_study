c2ef1ace9287203d96afd8c1ec375df7173acc81
seonguk070118 <seonguk070118@gmail.com>
2023-10-04 17:35:17 +0900

    10/4

--- study.java
+++ study.java
@@ -5,22 +5,33 @@ import java.util.Arrays;
 class A{
 	int a,b,c,d;
 	A(){
-		this(5);
+		this(3);
+		System.out.println("A持失切 1");
 	}
-	A(int k){
-		a=k;
-		b=k;
-		c=k;
-		d=k;
+	A(int a){
+		System.out.println("A持失切 2");
+	}
+}
+
+class B extends A{
+	B(){
+		this(3);
+		System.out.println("B持失切 1");
+	}
+	B(int a){
+		System.out.println("B持失切 2");
 	}
 }
 
 public class study {
 	public static void main(String[] ar) {
-		A a=new A();
-		System.out.println(a.a);
-		System.out.println(a.b);
-		System.out.println(a.c);
-		System.out.println(a.d);
+		A aa1 = new A();
+		System.out.println();
+		A aa2 = new A(3);
+		System.out.println();
+		
+		B bb1 = new B();
+		System.out.println();
+		B bb2 = new B(3);
 	}
 }

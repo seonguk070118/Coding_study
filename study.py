467fd8ab6d77105979a5ed9f936a5ee30658c3f6
seonguk070118 <seonguk070118@gmail.com>
2023-10-09 22:53:41 +0900

    6071_codeup

--- study.py
+++ study.py
@@ -1,11 +1,5 @@
-n=int(input())
-if n>=90:
-    print('A')
-else:
-    if n>=70:
-        print('B')
-    else:
-        if n>=40 :
-            print('C')
-        else:
-            print('D')
+n=1
+while n!=0:
+    n=int(input())
+    if n!=0:
+        print(n)

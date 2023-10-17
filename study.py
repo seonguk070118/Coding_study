bb637eac65e92f67bc8bdcb0099edf01a50ff2b3
seonguk070118 <seonguk070118@gmail.com>
2023-10-17 11:48:39 +0900

    10/17

--- study.py
+++ study.py
@@ -1,4 +1,10 @@
-n='s'
-while n!='q':
-    n=str(input())
-    print(n)
+class bssm:
+    def __init__(self,name,age,dow):
+        self.name = name
+        self.age = age
+        self.dow = dow
+    def intro(self):
+        print(f"안녕하세요, 부소마에서 {self.dow}를 담당하고 있는 {self.age}살 {self.name}입니다.")
+
+a=bssm('최성욱',17,'1116')
+a.intro()

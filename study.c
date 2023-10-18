2190fc14bce7402c1c35f28aa6fb5462f968f980
seonguk070118 <seonguk070118@gmail.com>
2023-10-18 09:23:03 +0900

    10/18_1

--- study.c
+++ study.c
@@ -2,20 +2,36 @@
 #include <stdlib.h>
 #include<limits.h>
 #include <stdbool.h>
-
-
+int i,k,n=8;
+int hash(int key)
+{
+    return key%n;
+}
 int main()
 {
-    int n,i,a[101],max=0,min=1000;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
+    int key;
+    int list[8]={0,0,10,3,2,5,0,0};
+    scanf("%d",&key);
+    int index=hash(key);
+    int t=index;
+    while(1)
     {
-    	scanf("%d",&a[i]);
-    	if(a[i]>max)
-    		max=a[i];
-    	if(a[i]<min)
-    		min=a[i];
-	}
-	printf("%d",max-min);
+        if(list[index]==key)
+        {
+            //list[index]=key;
+            break;
+        }
+        else
+        {
+            k++;
+            index=(hash(key)+k)%n;
+            if(t==index) 
+			{
+				printf("Å½»ö ½ÇÆÐ"); 
+				break;
+			}
+        }
+    }
+    printf("%d",index);
     return 0;
 }

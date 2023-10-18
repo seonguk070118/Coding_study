8f3da4dd9432a95a879f772ed6317b45d3271a81
seonguk070118 <seonguk070118@gmail.com>
2023-10-18 09:48:22 +0900

    10/18_이중해싱법

--- study.c
+++ study.c
@@ -3,6 +3,11 @@
 #include<limits.h>
 #include <stdbool.h>
 int i,k,n=8;
+int doublehash(int key)
+{
+	if(key>20) return 4;
+	else return 5;
+}
 int hash(int key)
 {
     return key%n;
@@ -13,7 +18,6 @@ int main()
     int list[8]={0,0,10,3,2,5,0,0};
     scanf("%d",&key);
     int index=hash(key);
-    int t=index;
     while(1)
     {
         if(list[index]==0)
@@ -24,7 +28,7 @@ int main()
         else
         {
             k++;
-            index=(hash(key)+k*k)%n;
+            index=(hash(key)+doublehash(key)*k)%n;
         }
     }
     printf("%d",index);

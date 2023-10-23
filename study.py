910fbb64eb008e6837b3e3c725494ccb1f578a12
seonguk070118 <seonguk070118@gmail.com>
2023-10-23 20:20:21 +0900

    6081_codeup

--- study.py
+++ study.py
@@ -1,11 +1,4 @@
-n=int(input())
-
-for i in range(1,n+1):
-    if i%10==3:
-        print("X",end=' ')
-    elif i%10==6:
-        print("X",end=' ')
-    elif i%10==9:
-        print("X",end=' ')
-    else:
-        print(i,end=' ')
+n = int(input(), 16)
+for i in range(1, 16):
+    result = n * i
+    print(f'{n:X}*{i:X}={result:X}')

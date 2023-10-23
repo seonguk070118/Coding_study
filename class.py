80b0a9f60ffbb83efb6327f6b12d62a0406aad7c
seonguk070118 <seonguk070118@gmail.com>
2023-10-23 20:20:35 +0900

    10/23

--- class.py
+++ class.py
@@ -1,10 +1,33 @@
+#1번문제
 n=int(input())
-list_test=list(map(int,input().split()))
+def prnt(n):
+    if(n<1):
+        return
+    print(n,end=' ')
+    prnt(n-1)
+    return
+prnt(n)
 
+
+#2번 문제
+n=int(input())
+l=list(map(int,input().split()))
 def Sum(n):
     if(n<0):
         return 0
-    s=list_test[n]+Sum(n-1)
+    s=l[n]+Sum(n-1)
     return s
-
 print(Sum(n-1))
+
+
+#3번문제
+def M(n):
+    if(n<0):
+        return 0
+    if(l[n]>M(n-1)):
+        return l[n]
+    else:
+        return M(n-1)
+n=int(input())
+l=list(map(int,input().split()))
+print(M(n-1))

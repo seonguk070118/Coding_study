b1dc992d4fac5964c5c346c5db62360d896cabbf
seonguk070118 <seonguk070118@gmail.com>
2023-11-02 17:40:50 +0900

    11/2

--- study.c
+++ study.c
@@ -3,31 +3,20 @@
 #include <string.h>
 #include<limits.h>
 #include <stdbool.h>
-// 동적으로 구조체 생성하기
-#include<stdio.h>
-#include<stdlib.h>
-typedef int element;
-//1)Node 구조체를 전역에 정의
-typedef struct Node{
-	element data;
-	struct Node *next;
-}Node;
-int main(){
-	 Node *n1, *n2 ,*n3, *head;
-    n1 = (Node *)malloc(sizeof(Node));//Node구조체 n1 동적할당
-    n2 = (Node *)malloc(sizeof(Node));//Node구조체 n2 동적할당
-		n3 = (Node *)malloc(sizeof(Node));//Node구조체 n3 동적할당
-    n1->data=10;//2) n1의 데이터에 10을 저장
-    n1->next=n2;//3) n1의 다음 노드 주소 next에 n2를 저장
- 		n2->data=20;//4) n2의 데이터에 20을 저장
-		n2->next=n3;//5)n2의 다음 노드 주소 next에 n3 저장
-  	n3->data=30;//6) n3의 데이터에 30을 저장
-		n3->next=NULL;//7) n3의 다음 노드 주소 next에 NULL 저장
-    head = n1;//8)n1의 주소 저장(참조)
-    while( head ){
-        printf("%d->", head->data);
-       head=head->next;//9) head에 다음 노드 주소 저장
-			  
-    }
-}
 
+int main(){
+	int a[101],n,max=0,count=0,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	max=a[n-1];
+	for(i=n-2;i>=0;i--){
+		if(a[i]>max){
+			count++;
+			max=a[i];
+		}
+	}
+	printf("%d",count);
+	return 0;
+}

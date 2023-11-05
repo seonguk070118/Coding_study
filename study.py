ee75645405f84f27fb98b1fb6c4de45a907bbeb0
seonguk070118 <seonguk070118@gmail.com>
2023-11-05 23:12:39 +0900

    1288_codeup

--- study.py
+++ study.py
@@ -1,5 +1,8 @@
-a,b,c=map(int,input().split())
-d=1
-while d%a!=0 or d%b!=0 or d%c!=0:
-    d+=1
-print(d)
+def aa(n):
+    if n<=1:
+        return 1
+    else:
+        return n*aa(n-1)
+
+c,b=map(int,input().split())
+print(aa(c)//(aa(b)*aa(c-b)))

59cc3da19fa556ea92fd446b5f5bce432e2e3089
seonguk070118 <seonguk070118@gmail.com>
2023-11-10 11:16:32 +0900

    1564_codeup_2

--- study.py
+++ study.py
@@ -1,4 +1,11 @@
-a,b=map(int,input().split())
+'''a,b=map(int,input().split())
 while(b!=0):
     a,b=b,a%b
-print(a)
+print(a)'''
+
+a,b=map(int,input().split())
+def f(a,b):
+    if(b==0):
+        return a
+    return f(b,a%b)
+print(f(a,b))

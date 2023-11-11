b20497cc491a99b024de9a5750adb0c73bfbc591
seonguk070118 <seonguk070118@gmail.com>
2023-11-11 23:12:40 +0900

    1291_codeup

--- study.py
+++ study.py
@@ -1,8 +1,6 @@
-n=int(input())
-l=[50000,10000,5000,1000,500,100,50,10]
-s=0
-for i in l:
-    while(n>=i):
-        n=n-i
-        s=s+1
+a,b,c=map(int,input().split())
+m=min(a,b,c)
+for i in range(1,m+1):
+    if a%i==0 and b%i==0 and c%i==0:
+        s=i
 print(s)

da8384add0c780cfa48b2b94479e498290c937af
seonguk070118 <seonguk070118@gmail.com>
2023-11-12 23:34:20 +0900

    1292_codeup

--- study.py
+++ study.py
@@ -1,6 +1,9 @@
-a,b,c=map(int,input().split())
-m=min(a,b,c)
-for i in range(1,m+1):
-    if a%i==0 and b%i==0 and c%i==0:
-        s=i
-print(s)
+a=int(input())
+s=0
+for i in range(1,11):
+    s=s+(a%10)
+    a=a//10
+if s%7==4:
+    print('suspect')
+else:
+    print('citizen')

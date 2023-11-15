f28c3ed3fa09bce2510391802d081255e2e76144
seonguk070118 <seonguk070118@gmail.com>
2023-11-15 17:49:42 +0900

    11/15_5

--- study.java
+++ study.java
@@ -2,20 +2,46 @@ package mypack;
 
 import java.util.Arrays;
 
-class bank{
-	static int money=5;
-	void addMoney(){
-		money++;
+class telephone{
+	void call(){
+		System.out.println("전화걸기");
+	}
+	
+	
+}
+
+class smartphone extends telephone {
+	@Override
+	void call() {
+		System.out.println("전화걸기\n녹음가능");
+	}
+	void camera() {
+		System.out.println("찰칵");
+	}
+}
+
+class internetphone extends telephone {
+	void internet() {
+		System.out.println("인터넷 연결");
+	}
+}
+
+class phone extends telephone {
+	void SMS() {
+		System.out.println("문자 발송합니다");
 	}
 }
 
 public class study {
+
 	public static void main(String[] ar) {
-		bank a = new bank();
-		System.out.println(a.money);
-		a.addMoney();
-		a.addMoney();
-		a.addMoney();
-		System.out.println(a.money);
+		telephone[] a = new telephone[] { 
+				new smartphone(), 
+				new internetphone(), 
+				new phone()
+		};
+		for(telephone t: a) {
+			t.call();
+		}
 	}
 }

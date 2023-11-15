85d8a4a8d5862a8689d3dbb06134ccc2941ae2a4
seonguk070118 <seonguk070118@gmail.com>
2023-11-15 17:19:40 +0900

    11/15_4

--- study.java
+++ study.java
@@ -2,32 +2,20 @@ package mypack;
 
 import java.util.Arrays;
 
-class Rectangle {
-	int width;
-	int height;
-	Rectangle(){
-		this(3,3);
-	}
-	Rectangle(int w){
-		this(w,5);
-	}
-	Rectangle(int w,int h){
-		this.width=w;
-		this.height=h;
-	}
-	void print() {
-		System.out.print(width + " ");
-		System.out.println(height);
+class bank{
+	static int money=5;
+	void addMoney(){
+		money++;
 	}
 }
 
 public class study {
 	public static void main(String[] ar) {
-		Rectangle a = new Rectangle();
-		Rectangle a1 = new Rectangle(16);
-		Rectangle a2 = new Rectangle(16,11);
-		a.print();
-		a1.print();
-		a2.print();
+		bank a = new bank();
+		System.out.println(a.money);
+		a.addMoney();
+		a.addMoney();
+		a.addMoney();
+		System.out.println(a.money);
 	}
 }

d714b96c9a05aee5368c8ce85f18cdf4f8762272
seonguk070118 <seonguk070118@gmail.com>
2023-11-15 17:15:13 +0900

    11/15_3

--- study.java
+++ study.java
@@ -1,18 +1,33 @@
 package mypack;
 
 import java.util.Arrays;
+
+class Rectangle {
+	int width;
+	int height;
+	Rectangle(){
+		this(3,3);
+	}
+	Rectangle(int w){
+		this(w,5);
+	}
+	Rectangle(int w,int h){
+		this.width=w;
+		this.height=h;
+	}
+	void print() {
+		System.out.print(width + " ");
+		System.out.println(height);
+	}
+}
+
 public class study {
 	public static void main(String[] ar) {
-		int a[] = new int[] {4,100,20,1,50,7};
-		for(int i: a) {
-			System.out.println(i);
-			if(i<a.length) {
-				System.out.println("원소가 작습니다");
-			}
-			else {
-				System.out.println("원소가 큽니다");
-			}
-		}
-		
+		Rectangle a = new Rectangle();
+		Rectangle a1 = new Rectangle(16);
+		Rectangle a2 = new Rectangle(16,11);
+		a.print();
+		a1.print();
+		a2.print();
 	}
 }

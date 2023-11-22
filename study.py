06e5079667c5c8579d583d947183f5b55875fdf8
seonguk070118 <seonguk070118@gmail.com>
2023-11-22 20:26:41 +0900

    1378_codeup

--- study.py
+++ study.py
@@ -1,22 +1,6 @@
-n,m,d=input().split()
-n=int(n)
-m=int(m)
-d=str(d)
-a=n-1
-b=1
-if d=='R':
-    for i in range(n):
-        for j in range(a):
-            print(end=' ')
-        for j in range(m):
-            print('*',end='')
-        a-=1
-        print()
-elif d=='L':
-    for i in range(n):
-        for j in range(m):
-            print('*',end='')
-        print()
-        for j in range(b):
-            print(end=' ')
-        b+=1
+n=int(input())
+su=0
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        su+=j
+print(su)

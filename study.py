ee7922735fe46b1170ddf9c1ba2992cb543ce8f1
seonguk070118 <seonguk070118@gmail.com>
2023-11-24 23:33:26 +0900

    1461_codeup

--- study.py
+++ study.py
@@ -1,2 +1,14 @@
-n=input()
-print(ord(n))
+n=int(input())
+a=[[0 for j in range(1,n+2)] for i in range(1,n+2)]
+b=n
+for i in range(1,n+1):
+    b=n*i
+    for j in range(1,n+1):
+        a[i][j]=b
+        b-=1
+    
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(a[i][j],end=' ')
+    print()

3a1aa11ceff5a8a96d8259c8f978b59c8ade1ed7
seonguk070118 <seonguk070118@gmail.com>
2023-11-25 22:39:46 +0900

    1417_codeup

--- study.py
+++ study.py
@@ -1,14 +1,4 @@
-n=int(input())
-a=[[0 for j in range(1,n+2)] for i in range(1,n+2)]
-b=n
-for i in range(1,n+1):
-    b=n*i
-    for j in range(1,n+1):
-        a[i][j]=b
-        b-=1
-    
-
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(a[i][j],end=' ')
-    print()
+a=list(map(int,input().split()))
+for i in range(2):
+    a.remove(max(a))
+print(max(a))

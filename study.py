bddd61549da9d69331017dbb2eb48dc5dc263963
seonguk070118 <seonguk070118@gmail.com>
2023-11-28 23:30:35 +0900

    1464_codeup

--- study.py
+++ study.py
@@ -1,11 +1,12 @@
-n=int(input())
-a=[[0 for j in range(1,n+2)] for i in range(1,n+2)]
-b=n
-for i in range(n):
-    for j in range(n):
-        a[i][j]=n+(j*n)-i
+m,n=map(int,input().split())
+a=[[0 for j in range(1,n+2)] for i in range(1,m+2)]
+b=m*n
+for i in range(1,m+1):
+    for j in range(1,n+1):
+        a[i][j]=b
+        b-=1
 
-for i in range(n):
-    for j in range(n):
+for i in range(1,m+1):
+    for j in range(1,n+1):
         print(a[i][j],end=' ')
     print()

1efc7ce5ff3aac7ad0f32d5f69218df4c006694d
seonguk070118 <seonguk070118@gmail.com>
2023-12-06 01:08:26 +0900

    12/5_3

--- study.java
+++ study.java
@@ -1,33 +1,19 @@
 package mypack;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-
-class A{
-	void abc() {
-		System.out.println("A 클래스 메서드 abc()");
-		class D{
-			
-		}
-	}
-	class B{
-		void bcd() {
-			System.out.println("B클래스 메서드 bcd()");
-			abc();
-		}
-	}
-	static class C{
-		
-	}
-}
 
 public class study {
 
 	public static void main(String[] ar) {
-		A a = new A();
-		A.B b=a.new B();
-		A.C c=new A.C();
-		a.abc();
-		b.bcd();
+		InputStreamReader is = null;
+		try {
+			is = new InputStreamReader(System.in);
+			System.out.println("입력글자 = "+is.read());
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
 

a715cffd8469d6eaaa5bc3cddb8dfc8471d7a365
seonguk070118 <seonguk070118@gmail.com>
2023-12-06 08:16:28 +0900

    12/5_

--- study.java
+++ study.java
@@ -1,20 +1,49 @@
-package mypack;
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
-public class study {
-
-	public static void main(String[] ar) {
-		InputStreamReader is = null;
-		try {
-			is = new InputStreamReader(System.in);
-			System.out.println("입력글자 = "+is.read());
-		}catch(IOException e) {
-			e.printStackTrace();
+	package mypack;
+	
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	import java.util.Arrays;
+	class DataBox{
+		boolean isEmpty=true;
+		int data;
+		synchronized void inputData(int data) {
+			if(!isEmpty) {
+				try {wait();}catch(InterruptedException e) {}
+			}
+			this.data=data;
+			isEmpty=false;
+			System.out.println("입력 데이터: "+data);
+			notify();
+		}
+		synchronized void outputData() {
+			if(isEmpty) {
+				try {wait();}catch(InterruptedException e) {}
+			}
+			isEmpty=true;
+			System.out.println("출력 데이터: "+data);
+			notify();
 		}
 	}
-}
-
-
+	public class study {
+		public static void main(String[] ar) {
+			DataBox dataBox = new DataBox();
+			Thread t1=new Thread() {
+				public void run() {
+					for(int i =1;i<10;i++) {
+						dataBox.inputData(i);
+					}
+				};
+			};
+			Thread t2=new Thread() {
+				public void run() {
+					for(int i =1;i<10;i++) {
+						dataBox.outputData();
+					}
+				};
+			};
+			t1.start();
+			t2.start();
+		}
+	}
+	
+	

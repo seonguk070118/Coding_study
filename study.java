14bd4c6d27a12912b45e4671a926312f9a167385
seonguk070118 <seonguk070118@gmail.com>
2023-12-06 00:12:49 +0900

    12/5_1

--- study.java
+++ study.java
@@ -2,46 +2,25 @@ package mypack;
 
 import java.util.Arrays;
 
-class telephone{
-	void call(){
-		System.out.println("전화걸기");
-	}
-	
-	
-}
-
-class smartphone extends telephone {
-	@Override
-	void call() {
-		System.out.println("전화걸기\n녹음가능");
-	}
-	void camera() {
-		System.out.println("찰칵");
+interface GPU{
+	public abstract void start();
+	public default void gamemode() {
+		System.out.println("Gamemode On");
 	}
 }
 
-class internetphone extends telephone {
-	void internet() {
-		System.out.println("인터넷 연결");
+class LGGPU implements GPU{
+	public void start() {
+		System.out.println("LG GPU start");
 	}
 }
 
-class phone extends telephone {
-	void SMS() {
-		System.out.println("문자 발송합니다");
-	}
-}
 public class study {
 
 	public static void main(String[] ar) {
-		telephone[] a = new telephone[] { 
-				new smartphone(), 
-				new internetphone(), 
-				new phone()
-		};
-		for(telephone t: a) {
-			t.call();
-		}
+		GPU a = new LGGPU();
+		a.start();
+		a.gamemode();
 	}
 }
 

b191a4fcb708f15c6e407f8fdc3e56bea7f9dc0f
seonguk070118 <seonguk070118@gmail.com>
2023-12-19 11:03:23 +0900

    10818_baekjoon

--- study.py
+++ study.py
@@ -1,5 +1,10 @@
-a,b=map(int,input().split())
+n=int(input())
 c=input().split()
+mx = -1000000
+mi = 1000000
 for i in c:
-    if int(i)<b:
-        print(i,end=' ')
+    if int(i)>=int(mx):
+        mx=i
+    if int(i)<=int(mi):
+        mi=i
+print(f'{mi} {mx}')

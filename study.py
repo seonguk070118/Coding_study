5a04c95a44b03fecb9c851c37f0a029023d437e6
seonguk070118 <seonguk070118@gmail.com>
2023-12-20 23:14:24 +0900

    9086_baekjoon

--- study.py
+++ study.py
@@ -1,10 +1,5 @@
-n=int(input())
-c=input().split()
-mx = -1000000
-mi = 1000000
-for i in c:
-    if int(i)>=int(mx):
-        mx=i
-    if int(i)<=int(mi):
-        mi=i
-print(f'{mi} {mx}')
+t=int(input())
+for i in range(t):
+    n=input()
+    print(n[0],end='')
+    print(n[-1])

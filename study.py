d4f81cb24c395f0577b43da8bdf0682a16c96252
seonguk070118 <seonguk070118@gmail.com>
2023-12-24 22:35:46 +0900

    ㅎㅎ

--- study.py
+++
@@ -1,8 +0,0 @@
-n,m=map(int,input().split())
-l=[0 for i in range(1,n+21)]
-for i in range(m):
-    a,b,c=map(int,input().split())
-    for j in range(a,b+1):
-        l[j] = c
-for i in range(1,n+1):
-    print(l[i],end=' ')

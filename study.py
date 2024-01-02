c57492f5f817892207e359bd9872e6eaf05a1a26
seonguk070118 <seonguk070118@gmail.com>
2024-01-03 08:49:10 +0900

    25083_baekjoon

--- study.py
+++ study.py
@@ -1,14 +1,6 @@
-a,b=map(list,input().split())
-a.reverse()
-b.reverse()
-for i in range(3):
-    if a[i]==b[i]:
-        continue
-    elif a[i]>b[i]:
-        for i in range(3):
-            print(a[i],end='')
-        break
-    else:
-        for i in range(3):
-            print(b[i],end='')
-        break
+print('         ,r\'"7')
+print('r`-_   ,\'  ,/')
+print(' \\. ". L_r\'')
+print('   `~\\/')
+print('      |')
+print('      |')

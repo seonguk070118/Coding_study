a34632593d0322f453a60b68eea79d0ca5ae600c
seonguk070118 <seonguk070118@gmail.com>
2024-01-10 23:16:52 +0900

    1271_baekjoon

--- study.py
+++ study.py
@@ -1,10 +1,3 @@
-sum=0
-while 1:
-    a=input()
-    if a=='#':
-        break
-    for i in a:
-        if i in 'aoieuAOIEU':
-            sum+=1
-    print(sum)
-    sum=0
+n,m=map(int,input().split())
+print(n//m)
+print(n%m)

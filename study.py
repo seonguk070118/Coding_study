2163b0946cf0bcf95f5d3600c9259f5c5cce7174
seonguk070118 <seonguk070118@gmail.com>
2024-01-13 20:49:02 +0900

    2083_baekjoon

--- study.py
+++ study.py
@@ -1,3 +1,12 @@
-a=int(input())
-for i in range(1,a+1):
-    print(i)
+while 1:
+    n=input()
+    if n=='# 0 0':
+        break
+    ns=n.split()
+    print(ns[0],end=' ')
+    if int(ns[1])>17:
+        print('Senior')
+    elif int(ns[2])>79:
+        print('Senior')
+    else:
+        print('Junior')

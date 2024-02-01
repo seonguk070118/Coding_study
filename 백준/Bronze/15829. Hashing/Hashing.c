66f8997af082ea5423142710a590b837b488963b
최성욱 <128461595+seonguk070118@users.noreply.github.com>
2024-02-01 16:49:51 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.c
+++ 백준/Bronze/15829. Hashing/Hashing.c
@@ -3,19 +3,20 @@
 #include <string.h>
 #include <limits.h>
 #include <stdbool.h>
-char a[51];
+char a[55];
 int main() {
     unsigned long long s=0,t=1;
     int i,j,n;
     scanf("%d",&n);
     scanf("%s",a);
-    for(i=0;i<strlen(a);i++){
+    for(i=0;i<n;i++){
     	for(j=0;j<i;j++){
     		t=t*31;
+    		t%=1234567891;
 		}
     	s+=(a[i]-96)*t;
     	t=1;
 	}
-	printf("%llu",s);
+	printf("%llu",s%1234567891);
 	return 0;
 }

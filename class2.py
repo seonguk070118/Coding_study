1dbedd80f1f0074e6b7d747f5b492493fdd99282
seonguk070118 <seonguk070118@gmail.com>
2024-03-19 11:28:45 +0900

    3/19_1

--- class2.py
+++ class2.py
@@ -1,6 +1,12 @@
-import cv2 as cv
-img = cv.imread('./image/cute.jpg')
-cv.imshow('image',img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+from socket import *
+Host = '127.0.0.1'
+Port = 9999
 
+client_socket = socket(AF_INET,SOCK_STREAM)
+
+client_socket.connect((Host,Port))
+client_socket.sendall('안녕!!!'.encode())
+
+data = client_socket.recv(1024)
+print('Received from',repr(data.decode()))
+client_socket.close()

156faa2b1e27854b76adace377e3e9a9406ee22a
seonguk070118 <seonguk070118@gmail.com>
2024-04-16 11:59:21 +0900

    산술 및 논리 연산

--- 빅데이터/data_class.py
+++ 빅데이터/data_class.py
@@ -2,19 +2,20 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv.imread('puppy.jpg',cv.IMREAD_GRAYSCALE)
-assert img is not None, "file could not be read, check with os.path.exists()"
+img1 = cv.imread('images/img1.jpg')
+img2 = cv.imread('images/img2.jpg')
 
-img = cv.medianBlur(img,5)
+img3 = cv.add(img1,img2)
 
-ret,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
-th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,11,2)
-th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
+img4 = cv.addWeighted(img1,0.5,img2,0.5,0)
+titles = ['src','map','add','addWeight']
 
-titles = ['Original Image','Global Thresholding (v = 127)','Adaptive Mean Thresholding','Adaptive Gaussian Thresholding']
-images = [img,th1,th2,th3]
+imgs = [img1,img2,img3,img4]
+
 for i in range(4):
-    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.subplot(2,2,i+1)
+    plt.imshow(imgs[i])
     plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
-plt.show()
+    plt.xticks([])
+    plt.yticks([])
+plt.show

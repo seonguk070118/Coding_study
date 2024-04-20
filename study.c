5cd80905e6611cccbb1a0f45d2965a5f60dc4c05
seonguk070118 <seonguk070118@gmail.com>
2024-04-20 22:49:50 +0900

    a

--- study.c
+++ study.c
@@ -4,52 +4,40 @@
 #include <limits.h>
 #include <stdbool.h>
 
-typedef struct student {
-    int num;
-    char name[20];
-    int mic, lin, pro;
-    int tot;
-    double avg;
-    char grade;
-} Student;
+#include<stdio.h>
+#include<othread.h>
 
-void input_data(Student *pary) {
-	int i;
-    for (i = 0; i < 1; i++) {
-        scanf("%d", &pary[i].num);
-        scanf("%s", pary[i].name);
-        scanf("%d", &pary[i].mic);
-        scanf("%d", &pary[i].lin);
-        scanf("%d", &pary[i].pro);
-    }
+void *produce(void *arg);
+void *consume(void *arg);
+
+int sum = 10;
+
+int main(){
+	printf("초기 합계: %d\n",sum);
+	
+	pthread_t producer, consumer;
+	
+	pthread_create(&producer, NULL,produce, NULL);
+	pthread_create(&consumer, NULL,consume, NULL);
+	
+	pthread_join(producer,NULL);
+	pthread_join(consumer,NULL);
+	
+	printf("생성자, 소비자 스레드 실행 이후 합계: %d\n",sum);
+	
+	return 0;
 }
 
-void calc_data(Student *pary) {
-	int i;
-    for (i = 0; i < 1; i++) {
-        pary[i].tot = pary[i].mic + pary[i].lin + pary[i].pro;
-        pary[i].avg = pary[i].tot / 3.0;
-        
-        if (pary[i].avg >= 90) pary[i].grade = 'A';
-        else if (pary[i].avg >= 80) pary[i].grade = 'B';
-        else if (pary[i].avg >= 70) pary[i].grade = 'C';
-        else pary[i].grade = 'F';
-    }
+void *produce(void *arg){
+	for(int i = ; i<1000000;i++){
+		sum--;
+	}
+	pthread_exit(NULL);
 }
-
-void print_data(Student *pary) {
-	int i;
-    for (i = 0; i < 1; i++) {
-        printf("%d, %s, %d, %d, %d, %d, %.0f, %c\n", pary[i].num, pary[i].name, pary[i].mic, pary[i].lin, pary[i].pro, pary[i].tot, pary[i].avg, pary[i].grade);
-    }
-}
-
-int main(void) {
-    Student ary[5];
-    input_data(ary);
-    calc_data(ary);
-    print_data(ary);
-    return 0;
-
+void *consume(void *arg){
+	for(int i = ; i<1000000;i++){
+		sum--;
+	}
+	pthread_exit(NULL);
 }
 

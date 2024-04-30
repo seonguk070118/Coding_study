f12430a61cb9114ed3aa7399d618e457e2c6438c
seonguk070118 <seonguk070118@gmail.com>
2024-04-30 11:07:43 +0900

    영상 명암비 조절

--- 빅데이터/data_class.py
+++ 빅데이터/data_class.py
@@ -1,46 +1,50 @@
-import cv2 as cv2
-CAMERA_ID = 0
-cam = cv2.VideoCapture(CAMERA_ID)
-if cam.isOpened() == False:
-    print
-    'Cannot open the camera-%d' % (CAMERA_ID)
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img1 = cv2.imread("./images/img5.jpg", cv2.IMREAD_GRAYSCALE)
+
+if img1 is None:
+    print('no file found')
     exit()
 
-cv2.namedWindow('CAM Window')
+multi_lut = np.full(shape=[256], fill_value=0, dtype=np.uint8)
+log_lut = np.full(shape=[256], fill_value=0, dtype=np.uint8)
+invol_lut = np.full(shape=[256], fill_value=0, dtype=np.uint8)
+sel_lut = np.full(shape=[256], fill_value=0, dtype=np.uint8)
 
-background = None
+multi_v = 2; gamma1 = 0.1; gamma2=0.6
+thres1  = 5; thres2 = 100
 
-while(True):
-    ret,frame = cam.read()
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('CAM Window',gray)
-    key = cv2.waitKey(33)
-    if key == ord('q'):
-        break
-    if key == ord('a'):
-        background = gray.copy()
-        print('성공')
-        break
-c=-1
-while(True):
-    ret,frame = cam.read()
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    diff = cv2.absdiff(gray,background)
-    key = cv2.waitKey(33)
-    
-    if key == ord('b'):
-        c*=-1
-        print(c)
-    if c == 1:
-        _, thresh = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
-        cv2.imshow('CAM Window', thresh)
-        key = cv2.waitKey(33)
-        
-    else:
-        cv2.imshow('CAM Window',diff)
-    if key == ord('q'):
-        break
+max_v_log = 255 / np.log(1+255)
+max_v_invol = 255 / np.power(255, gamma1)
+max_v_sel = 100 / np.power(thres2, gamma2)
 
+for i in range(256):
+    val = i * multi_v
 
-cam.release()
-cv2.destroyAllWindows()
+    if val >255 : val = 255
+    multi_lut[i] = val
+    log_lut[i] = np.round(max_v_log * np.log(1+i))
+    invol_lut[i] = np.round(max_v_invol * np.power(i,gamma1))
+
+    if i< thres1 : sel_lut[i]=i
+    elif i > thres2 : sel_lut[i]= i
+    else: sel_lut[i] = np.round(max_v_sel * np.power(i,gamma2))
+
+ress=[]
+ress.append(img1)
+ress.append(cv2.LUT(img1,multi_lut))
+ress.append(cv2.LUT(img1,log_lut))
+ress.append(cv2.LUT(img1,invol_lut))
+ress.append(cv2.LUT(img1,sel_lut))
+
+titles=['입력영상','상수곱','로그 변환','거듭제곱 변환', '구간 변환']
+
+plt.rc('font',family='malgun Gothic')
+for i in range(5):
+    plt.subplot(2,3,i+1)
+    plt.imshow(ress[i],cmap='gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()

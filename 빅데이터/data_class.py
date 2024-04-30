9717963c4813a91ee1b0bb57d3038c6e77316597
seonguk070118 <seonguk070118@gmail.com>
2024-04-30 12:14:40 +0900

    영상 블러링

--- 빅데이터/data_class.py
+++ 빅데이터/data_class.py
@@ -3,67 +3,25 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-img1 = cv2.imread("./images/img6.jpg", cv2.IMREAD_GRAYSCALE)
+img1 = cv2.imread("./images/img8.jpg", cv2.IMREAD_GRAYSCALE)
 
-res1 = cv2.equalizeHist(img1)
-ch1 = [0]; ranges1 = [0, 256]; histSize1 = [256]
-hist1 = cv2.calcHist([img1], ch1, None, histSize1, ranges1)
-hist2 = cv2.calcHist([res1], ch1, None, histSize1, ranges1)
+ksize1 = 3; ksize2 = 5; ksize3 = 7; ksize4 = 9
+kernel = np.full(shape=[ksize4,ksize4],fill_value=1,dtype=np.float32)/ksize4*ksize4
+res1 = cv2.blur(img1,(ksize1,ksize1))
+res2 = cv2.blur(img1,(ksize2,ksize2))
+res3 = cv2.boxFilter(img1,-1,(ksize3,ksize3))
+res4 = cv2.filter2D(img1,-1,kernel)
+res5 = cv2.boxFilter(img1,-1,(1,21))
 
-multi_lut = np.full(shape=[256], fill_value=0, dtype=np.uint8)
-log_lut = np.full(shape=[256], fill_value=0, dtype=np.uint8)
-invol1_lut = np.full(shape=[256], fill_value=0, dtype=np.uint8)
-multi_v = 2; gamma1 = 0.4
-thres1 = 5; thres2 = 100
-max_v_log = 255/ np.log(1+255)
-max_v_invol1 = 255 / np.power(255, gamma1)
+ress = []
+ress.append(img1),ress.append(res1),ress.append(res2)
+ress.append(res3),ress.append(res4),ress.append(res5)
 
-for i in range(256):
-    val = i * multi_v
-    if val > 255 : val = 255
-    multi_lut[i]=val
-    log_lut[i] = np.round(max_v_log * np.log(1+i))
-    invol1_lut[i] =  np.round(max_v_invol1 * np.power(i, gamma1))
+titles = ['input','res1','res2','res3','res4','res5']
 
-res2 = cv2.LUT(img1,multi_lut)
-res3 = cv2.LUT(img1,log_lut)
-res4 = cv2.LUT(img1,invol1_lut)
-
-hist3 = cv2.calcHist([res2],ch1, None,histSize1,ranges1)
-hist4 = cv2.calcHist([res3],ch1, None,histSize1,ranges1)
-hist5 = cv2.calcHist([res4],ch1, None,histSize1,ranges1)
-
-bin_x = np.arange(256)
-fig_index = 0
-
-ress=[]
-ress.append(img1)
-ress.append(res1)
-ress.append(res2)
-ress.append(res3)
-ress.append(res4)
-
-titles=["Input Histogram", "Equalization-convert Histogram","Multiply-convert Histogram","log-convert Histogram","Invil-convert Histogram"]
-
-plt.rc('font',family='malgun Gothic')
-for i in range(5):
+for i in range(6):
     plt.subplot(2,3,i+1)
     plt.imshow(ress[i],cmap='gray')
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
-plt.show()
-
-display_hist = [("Input Histogram",hist1),
-                ("Equalization-convert Histogram",hist2)
-                ("Multiply-convert Histogram",hist3)
-                ("log-convert Histogram",hist4)
-                ("Invil-convert Histogram",hist5)]
-
-for (name,out) in display_hist:
-    plt.figure(fig_index)
-    plt.title9name; plt.xlabel("Bin"); plt.ylabel("Frequency")
-    plt.bar(bin_x,out[:,0],width=6,color='g')
-    plt.grid(True,lw=1,ls='--',c='.75')
-    plt.xlim([0,255])
-    fig_index += 1
 plt.show()
